339b8395ba0d3d1fd8db6b80dcc391904e7beaaa
bitweft <bitweft@gmail.com>
2023-02-04 21:44:37 -0700

    Visit base url before every web scenario

--- config/wdio-web-chrome.conf.ts
+++ config/wdio-web-chrome.conf.ts
@@ -1,10 +1,20 @@
 import { config } from './wdio-shared.conf';
 
+const baseUrl = 'https://book-information-web.vercel.app/';
+
 config.services = ['chromedriver'];
 config.capabilities = [
   {
     browserName: 'chrome',
   }
 ]
+
+config.before = async(_, __, browser): Promise<void> => {
+  await browser.setWindowSize(1440, 1080);
+}
+
+config.beforeScenario = async(): Promise<void> => {
+  await browser.url(baseUrl);
+}
 
 module.exports.config = config;
