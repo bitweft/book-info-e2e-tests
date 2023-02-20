d0aee45bc93696142df5a699333e0ea32b68ec5f
bitweft <bitweft@gmail.com>
2023-02-19 18:12:07 -0700

    Add video reporter for chrome tests

--- config/wdio-web-chrome.conf.ts
+++ config/wdio-web-chrome.conf.ts
@@ -1,3 +1,4 @@
+import * as video from 'wdio-video-reporter';
 import { config } from './wdio-shared.conf';
 
 const baseUrl = 'https://book-information-web.vercel.app/';
@@ -9,11 +10,23 @@ config.capabilities = [
   }
 ]
 
-config.before = async(_, __, browser): Promise<void> => {
+// config.reporters?.push(
+//   [
+//     video,
+//     {
+//       saveAllVideos: false,
+//       videoSlowdownMultiplier: 5,
+//       videoRenderTimeout: 5,
+//       outputDir: 'artifacts',
+//     },
+//   ]
+// );
+
+config.before = async (_, __, browser): Promise<void> => {
   await browser.setWindowSize(1440, 1080);
 }
 
-config.beforeScenario = async(): Promise<void> => {
+config.beforeScenario = async (): Promise<void> => {
   await browser.url(baseUrl);
 }
 
