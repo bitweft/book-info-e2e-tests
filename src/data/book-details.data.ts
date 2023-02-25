3f830044adcd29d7d123fce5837c1b8351698454
bitweft <bitweft@gmail.com>
2023-02-25 13:20:00 -0700

    Add book details test data

---
+++ src/data/book-details.data.ts
@@ -0,0 +1,11 @@
+export type BookDetails = {
+  title: string;
+  description: string;
+};
+
+export const bookDetailsData: Record<string, BookDetails> = {
+  'Agile Testing': {
+    title: 'Agile Testing: A Practical Guide for Testers and Agile Teams',
+    description: 'In Agile Testing, Crispin and Gregory define agile testing and illustrate the tester’s role with examples from real agile teams. They teach you how to use the agile testing quadrants to identify what testing is needed, who should do it, and what tools might help.',
+  },
+};
